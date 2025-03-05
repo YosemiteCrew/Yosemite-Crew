6f186dba4d1152d98ef3dddac0697717504aa6ee
varun0817 <varun.immanentsolutions@gmail.com>
2025-03-05 02:24:51 -0800

    Merged Code

--- apps/mobileapp/src/app/constants/index.js
+++ apps/mobileapp/src/app/constants/index.js
@@ -1,2 +1,8 @@
-export const API_BASE_URL = '';
-export const GOOGLE_API_KEY = '';
+export const API_BASE_URL = 'https://devapi.yosemitecrew.com/api/';
+
+export const GOOGLE_API_KEY = 'AIzaSyBKuPqcVG84Pwqvb_nnPp5sYbs-mby9SuI';
+
+export const AWS_S3_KEY = 'AKIAU6GDZXAMYCYYHKPB';
+export const AWS_S3_SECRET = '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';
+export const AWS_S3_REGION = 'eu-central-1';
+export const AWS_S3_BUCKET = 'yosemitecrew';
